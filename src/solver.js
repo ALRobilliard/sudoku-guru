14581ff045c7f963a150c747fab1e19f9293b441
Andrew Robilliard <andrew.robilliard@intergen.co.nz>
2018-08-24 14:34:23 +1200

    Finish TS implementation

--- src/solver.js
+++ src/solver.js
@@ -9,70 +9,118 @@ var _grid = [
     [0, 0, 0, 0, 0, 0, 0, 7, 4],
     [0, 0, 5, 2, 0, 6, 3, 0, 0]
 ];
+function initGame(valStr) {
+    var valArr = valStr.split(',');
+    var output = [];
+    var rowCount = 0;
+    for (var index = 0; index < valArr.length; index++) { }
+}
 var square = /** @class */ (function () {
     function square(yPos, xPos, val) {
-        this.isSolved = false;
         this.yPos = yPos;
         this.xPos = xPos;
-        if (val > 0) {
-            this.value = val;
-            this.isSolved = true;
-            this.possibleValues = [];
-        }
+        this.value = val;
     }
+    // Checks whether another position in the same row contains the value.
     square.prototype.usedInRow = function (puzzle, num) {
-        for (var col = 0; col < puzzle.length; col++) {
-            if (puzzle[col][this.xPos] == num) {
+        for (var col = 0; col < 9; col++) {
+            if (puzzle[col][this.xPos].value == num) {
                 return true;
             }
         }
         return false;
     };
+    // Checks whether another position in the same column contains the value.
     square.prototype.usedInCol = function (puzzle, num) {
-        for (var row = 0; row < puzzle[this.yPos].length; row++) {
-            if (puzzle[this.yPos][row] == num) {
+        for (var row = 0; row < 9; row++) {
+            if (puzzle[this.yPos][row].value == num) {
                 return true;
             }
         }
         return false;
     };
+    // Checks whether another postion in the same 3x3 sub-grid contains the value.
     square.prototype.usedInBox = function (puzzle, num) {
-        for (var y = 0; y < Math.floor(Math.sqrt(puzzle.length)); y++) {
-            for (var x = 0; x < Math.floor(Math.sqrt(puzzle[y].length)); x++) {
-                if (puzzle[this.yPos + y][this.xPos + x] == num) {
+        var yMin = this.yPos - (this.yPos % 3);
+        var yMax = this.yPos + ((this.yPos + 1) % 3);
+        var xMin = this.xPos - (this.xPos % 3);
+        var xMax = this.xPos + ((this.xPos + 1) % 3);
+        for (var y = yMin; y < yMax; y++) {
+            for (var x = xMin; x < xMax; x++) {
+                if (puzzle[y][x].value == num) {
                     return true;
                 }
             }
         }
         return false;
     };
+    // Checks whether the number conflicts with any relevant existing squares.
     square.prototype.locationIsSafe = function (puzzle, num) {
-        return !(this.usedInCol(puzzle, num) &&
-            this.usedInRow(puzzle, num) &&
-            this.usedInBox(puzzle, num));
+        return (!this.usedInCol(puzzle, num) &&
+            !this.usedInRow(puzzle, num) &&
+            !this.usedInBox(puzzle, num));
     };
     return square;
 }());
 var sudokuGame = /** @class */ (function () {
     function sudokuGame(puzzle) {
         this.squares = [];
-        for (var y = 0; y < puzzle.length; y++) {
+        this.possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        for (var y = 0; y < 9; y++) {
             var row = [];
-            for (var x = 0; x < puzzle[y].length; x++) {
+            for (var x = 0; x < 9; x++) {
                 row.push(new square(y, x, puzzle[y][x]));
             }
             this.squares.push(row);
         }
+        if (this.solve()) {
+            this.printGrid();
+        }
+        else {
+            console.log('No solution to this puzzle exits.');
+        }
     }
     sudokuGame.prototype.printGrid = function () {
-        for (var y = 0; y < this.squares.length; y++) {
+        for (var y = 0; y < 9; y++) {
             var line = '';
-            for (var x = 0; x < this.squares[y].length; x++) {
+            for (var x = 0; x < 9; x++) {
                 var val = this.squares[y][x].value;
                 line += val != null ? val + ' ' : '  ';
             }
             console.log(line);
         }
+    };
+    sudokuGame.prototype.findEmpty = function () {
+        for (var y = 0; y < 9; y++) {
+            for (var x = 0; x < 9; x++) {
+                if (this.squares[y][x].value == 0) {
+                    this.backtrackIndex[0] = y;
+                    this.backtrackIndex[1] = x;
+                    return true;
+                }
+            }
+        }
+        return false;
+    };
+    sudokuGame.prototype.solve = function () {
+        this.backtrackIndex = [0, 0];
+        // If no empty squares exist then the puzzle is solved.
+        if (!this.findEmpty()) {
+            return true;
+        }
+        var col = this.backtrackIndex[0];
+        var row = this.backtrackIndex[1];
+        for (var _i = 0, _a = this.possibilities; _i < _a.length; _i++) {
+            var number = _a[_i];
+            if (this.squares[col][row].locationIsSafe(this.squares, number)) {
+                this.squares[col][row].value = number;
+                if (this.solve()) {
+                    return true;
+                }
+                this.squares[col][row].value = 0;
+            }
+        }
+        return false;
     };
     return sudokuGame;
 }());
--- src/solver.ts
+++ src/solver.ts
@@ -10,45 +10,52 @@ let _grid = [
   [0, 0, 5, 2, 0, 6, 3, 0, 0]
 ];
 
+function initGame() {
+  let vals: number[] = [];
+  let grid: number[][] = [];
+  new sudokuGame(grid);
+}
+
 class square {
   xPos: number;
   yPos: number;
   value: number;
-  isSolved: boolean = false;
-  possibleValues: number[];
 
   constructor(yPos: number, xPos: number, val: number) {
     this.yPos = yPos;
     this.xPos = xPos;
-    if (val > 0) {
-      this.value = val;
-      this.isSolved = true;
-      this.possibleValues = [];
-    }
+    this.value = val;
   }
 
-  usedInRow(puzzle: number[][], num: number): boolean {
-    for (let col = 0; col < puzzle.length; col++) {
-      if (puzzle[col][this.xPos] == num) {
+  // Checks whether another position in the same row contains the value.
+  usedInRow(puzzle: square[][], num: number): boolean {
+    for (let col = 0; col < 9; col++) {
+      if (puzzle[col][this.xPos].value == num) {
         return true;
       }
     }
     return false;
   }
 
-  usedInCol(puzzle: number[][], num: number): boolean {
-    for (let row = 0; row < puzzle[this.yPos].length; row++) {
-      if (puzzle[this.yPos][row] == num) {
+  // Checks whether another position in the same column contains the value.
+  usedInCol(puzzle: square[][], num: number): boolean {
+    for (let row = 0; row < 9; row++) {
+      if (puzzle[this.yPos][row].value == num) {
         return true;
       }
     }
     return false;
   }
 
-  usedInBox(puzzle: number[][], num: number): boolean {
-    for (let y = 0; y < Math.floor(Math.sqrt(puzzle.length)); y++) {
-      for (let x = 0; x < Math.floor(Math.sqrt(puzzle[y].length)); x++) {
-        if (puzzle[this.yPos + y][this.xPos + x] == num) {
+  // Checks whether another postion in the same 3x3 sub-grid contains the value.
+  usedInBox(puzzle: square[][], num: number): boolean {
+    let yMin = this.yPos - (this.yPos % 3);
+    let yMax = this.yPos + ((this.yPos + 1) % 3);
+    let xMin = this.xPos - (this.xPos % 3);
+    let xMax = this.xPos + ((this.xPos + 1) % 3);
+    for (let y = yMin; y < yMax; y++) {
+      for (let x = xMin; x < xMax; x++) {
+        if (puzzle[y][x].value == num) {
           return true;
         }
       }
@@ -56,36 +63,82 @@ class square {
     return false;
   }
 
-  locationIsSafe(puzzle: number[][], num: number): boolean {
-    return !(
-      this.usedInCol(puzzle, num) &&
-      this.usedInRow(puzzle, num) &&
-      this.usedInBox(puzzle, num)
+  // Checks whether the number conflicts with any relevant existing squares.
+  locationIsSafe(puzzle: square[][], num: number): boolean {
+    return (
+      !this.usedInCol(puzzle, num) &&
+      !this.usedInRow(puzzle, num) &&
+      !this.usedInBox(puzzle, num)
     );
   }
 }
 
 class sudokuGame {
   squares: square[][] = [];
+  backtrackIndex: number[];
+  possibilities: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   constructor(puzzle: number[][]) {
-    for (let y = 0; y < puzzle.length; y++) {
+    for (let y = 0; y < 9; y++) {
       let row: square[] = [];
-      for (let x = 0; x < puzzle[y].length; x++) {
+      for (let x = 0; x < 9; x++) {
         row.push(new square(y, x, puzzle[y][x]));
       }
       this.squares.push(row);
     }
+
+    if (this.solve()) {
+      this.printGrid();
+    } else {
+      console.log('No solution to this puzzle exits.');
+    }
   }
 
-  printGrid() {
-    for (let y = 0; y < this.squares.length; y++) {
+  printGrid(): void {
+    for (let y = 0; y < 9; y++) {
       let line = '';
-      for (let x = 0; x < this.squares[y].length; x++) {
+      for (let x = 0; x < 9; x++) {
         let val = this.squares[y][x].value;
         line += val != null ? val + ' ' : '  ';
       }
       console.log(line);
     }
+  }
+
+  findEmpty(): boolean {
+    for (let y = 0; y < 9; y++) {
+      for (let x = 0; x < 9; x++) {
+        if (this.squares[y][x].value == 0) {
+          this.backtrackIndex[0] = y;
+          this.backtrackIndex[1] = x;
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  solve(): boolean {
+    this.backtrackIndex = [0, 0];
+
+    // If no empty squares exist then the puzzle is solved.
+    if (!this.findEmpty()) {
+      return true;
+    }
+
+    let col = this.backtrackIndex[0];
+    let row = this.backtrackIndex[1];
+    for (let number of this.possibilities) {
+      if (this.squares[col][row].locationIsSafe(this.squares, number)) {
+        this.squares[col][row].value = number;
+
+        if (this.solve()) {
+          return true;
+        }
+
+        this.squares[col][row].value = 0;
+      }
+    }
+    return false;
   }
 }
